6620cf5468cc97605e50cafb26dd335cfc480c76
devkentron <dev@kentron.ai>
2025-04-14 12:59:30 +0530

    changes

--- components/ui/theme-provider.tsx
+++ components/ui/theme-provider.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps as NextThemeProviderProps } from "next-themes"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 type ThemeProviderProps = {
   children: React.ReactNode;
-} & NextThemeProviderProps
+  [key: string]: any;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
+} 
