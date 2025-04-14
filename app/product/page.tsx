85a1afc8aad288c673445fe5f12bd610994fd4c5
devkentron <dev@kentron.ai>
2025-04-14 12:28:51 +0530

    initial-commit

---
+++ app/product/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Content7 from '@/components/content-7'
+import Features7 from '@/components/features-7'
+import Features2 from '@/components/features-2'
+import Features6 from '@/components/features-6'
+import CallToAction from '@/components/call-to-action'
+const page = () => {
+  return (
+    <div>
+        <Content7 />
+        <Features7 />
+        <Features2 />
+        <Features6/>
+    </div>
+  )
+}
+
+export default page
