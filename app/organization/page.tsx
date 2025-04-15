9420ae8457176b220f9f38a1932ea6d029bd004c
devkentron <dev@kentron.ai>
2025-04-15 12:29:09 +0530

    changes

--- app/organization/page.tsx
+++ app/organization/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Content5 from '@/components/content-5'
 import Feature72 from '@/components/feature72'
+import CallToAction from '@/components/call-to-action'
 const page = () => {
   return (
     <div>
         <Content5 />
         <Feature72 />
+        <CallToAction />
     </div>
   )
 }
